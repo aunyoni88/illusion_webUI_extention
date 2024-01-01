8fe7d38320dd088b27567052e5325cc85fbf91ea
oni88 <arif@bitmorpher.com>
2024-01-01 19:54:52 +0600

    illusion extention run difussion package issue fixed version 0.25.0

--- scripts/api.py
+++ scripts/api.py
@@ -1,40 +1,13 @@
-# import gradio as gr
-# import time
-# from PIL import Image
-# from io import BytesIO
-# import base64
-# import io
-# import piexif
-# import piexif.helper
-# from datetime import datetime, timezone
-# from fastapi import FastAPI, Body
-# from time import gmtime, strftime
+from fastapi import FastAPI, Body
 
+from modules.api.models import *
+from modules.api import api
+import gradio as gr
+from datetime import datetime, timezone
+import time
 
 from app import inference
-from fastapi import FastAPI, Body, HTTPException
-from fastapi.responses import RedirectResponse, FileResponse
-from fastapi import File, UploadFile, Form
-import gradio as gr
-import time
-from datetime import datetime, timezone
-from pipeline_stable_diffusion_controlnet_inpaint import *
-from scratch_detection import ScratchDetection
-from diffusers import ControlNetModel, DEISMultistepScheduler
-from arif_install import downloadScratchRemoverModel
-from PIL import Image
-import cv2
-import glob
-import shutil
-import os
-from os.path import exists
-import subprocess
-import base64
-from io import BytesIO
-import  numpy
-from sympy import true, false
-
-# from extensions.illusion_webUI_extention.app import inference
+from main import decode_base64_to_image, encode_pil_to_base64
 
 
 def illusion_remove_api(_: gr.Blocks, app: FastAPI):
@@ -43,73 +16,42 @@ def illusion_remove_api(_: gr.Blocks, app: FastAPI):
             input_image: str = Body("", title='illusion input image'),
             prompt: str = Body("", title='prompt'),
     ):
-        # if not input_image:
-        #     return {
-        #         "success": False,
-        #         "message": "Input image not found",
-        #         "server_hit_time": '',
-        #         "server_process_time": '',
-        #         "output_image": ''
-        #     }
-        # utc_time = datetime.now(timezone.utc)
-        # start_time = time.time()
-        # print("time now: {0} ".format(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-        # input_image = decode_base64_to_image(input_image)
-        # image = inference(
-        #     control_image=input_image,
-        #     controlnet_conditioning_scale=1.2,  # illusion strength
-        #     prompt=prompt if prompt else "landscape of a forest, bright sky, vibrant colors",
-        #     negative_prompt='low quality',
-        #     guidance_scale=7.5,
-        #     sampler="Euler",  # Model or sampler "DPM++ Karras SDE"
-        #     control_guidance_start=0,
-        #     control_guidance_end=1.0,
-        #     upscaler_strength=1.0,
-        #     seed=-1,
-        #
-        # )
+        if not input_image:
+            return {
+                "success": False,
+                "message": "Input image not found",
+                "server_hit_time": '',
+                "server_process_time": '',
+                "output_image": ''
+            }
+        utc_time = datetime.now(timezone.utc)
+        start_time = time.time()
+        input_image = decode_base64_to_image(input_image)
+        image = inference(
+            control_image=input_image,
+            controlnet_conditioning_scale=1.2,  # illusion strength
+            prompt=prompt if prompt else "landscape of a forest, bright sky, vibrant colors",
+            negative_prompt='low quality',
+            guidance_scale=7.5,
+            sampler="Euler",  # Model or sampler "DPM++ Karras SDE"
+            control_guidance_start=0,
+            control_guidance_end=1.0,
+            upscaler_strength=1.0,
+            seed=-1,
 
-        # output_image = encode_pil_to_base64(image[0]).decode("utf-8")
-        #
-        # print("time taken: {0}".format(time.time() - start_time))
+        )
+
+        output_image = encode_pil_to_base64(image[0]).decode("utf-8")
+
+        print("time taken: {0}".format(time.time() - start_time))
 
         return {
-            "success": True
-            # "message": "Returned output successfully",
-            # "server_hit_time": str(utc_time),
-            # "server_process_time": time.time() - start_time,
-            # "output_image": output_image
+            "success": True,
+            "message": "Returned output successfully",
+            "server_hit_time": str(utc_time),
+            "server_process_time": time.time() - start_time,
+            "output_image": output_image
         }
-
-    # def decode_base64_to_image(img_string):
-    #     img = Image.open(BytesIO(base64.b64decode(img_string)))
-    #     return img
-
-    # def encode_pil_to_base64(image):
-    #     with io.BytesIO() as output_bytes:
-    #         if image.mode == "RGBA":
-    #             image = image.convert("RGB")
-    #         parameters = image.info.get('parameters', None)
-    #         exif_bytes = piexif.dump({
-    #             "Exif": {piexif.ExifIFD.UserComment: piexif.helper.UserComment.dump(parameters or "",
-    #                                                                                 encoding="unicode")}
-    #         })
-    #         image.save(output_bytes, format="JPEG", exif=exif_bytes)
-    #         bytes_data = output_bytes.getvalue()
-    #     return base64.b64encode(bytes_data)
-
-    # models = [
-    #     "None",
-    #     "u2net",
-    #     "u2netp",
-    #     "u2net_human_seg",
-    #     "u2net_cloth_seg",
-    #     "silueta",
-    #     "isnet-general-use",
-    #     "isnet-anime",
-    # ]
-
-
 
 
 try:
